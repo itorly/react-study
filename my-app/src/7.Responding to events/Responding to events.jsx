be2cb4a344d87ada0864720c81cc9035f1c311dc
itorly <itorly@163.com>
2025-06-16 15:14:34 +0800

    7.Responding to events

---
+++ my-app/src/7.Responding to events/Responding to events.jsx
@@ -0,0 +1,22 @@
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
+function RespondingToEvents() {
+  return (
+    <div>
+      <h1>7.Responding to events</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+export default RespondingToEvents;
--- my-app/src/QuickStartMain.jsx
+++ my-app/src/QuickStartMain.jsx
@@ -7,6 +7,7 @@ import AddingStyles from './3.Adding styles/3.Adding styles.jsx'
 import Profile from './4.Displaying data/Displaying data.jsx'
 import ConditionalRendering from './5.Conditional rendering/Conditional rendering.jsx'
 import RenderingLists from './6.Rendering lists/Rendering lists.jsx'
+import RespondingToEvents from './7.Responding to events/Responding to events.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -16,5 +17,6 @@ createRoot(document.getElementById('root')).render(
     <Profile />
     <ConditionalRendering />
     <RenderingLists />
+    <RespondingToEvents />
   </StrictMode>,
 )
