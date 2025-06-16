d4ee20b624a04d3f03ef642a0127bbc42c656d4a
itorly <itorly@163.com>
2025-06-16 13:56:19 +0800

    3.Adding styles

---
+++ my-app/src/3.Adding styles.jsx
@@ -0,0 +1,17 @@
+import './Adding styles.css'
+import imgExample from './img/smile.jpg'
+
+function MyImg() {
+  return (
+    <img className="avatar" src={imgExample} />
+  );
+}
+
+export default function AddingStyles() {
+    return (
+      <div>
+        <h1>3. Writing markup with JSX</h1>
+        <MyImg />
+      </div>
+    );
+}
--- my-app/src/QuickStartMain.jsx
+++ my-app/src/QuickStartMain.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 import MyApp from './1.Creating and nesting components.jsx'
 import AboutPage from './2.Writing markup with JSX.jsx'
+import AddingStyles from './3.Adding styles.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MyApp />
     <AboutPage />
+    <AddingStyles />
   </StrictMode>,
 )
