d9cfb07b7bf7977b3bd27ec5fd903071a7e1311f
itorly <itorly@163.com>
2025-06-16 13:30:30 +0800

    2.Writing markup with JSX

--- my-app/src/QuickStartMain.jsx
+++ my-app/src/QuickStartMain.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import MyApp from './Creating and nesting components.jsx'
+import AboutPage from './Writing markup with JSX.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MyApp />
+    <AboutPage />
   </StrictMode>,
 )
---
+++ my-app/src/Writing markup with JSX.jsx
@@ -0,0 +1,11 @@
+function AboutPage() {
+  return (
+    <>
+      <h1>2. Writing markup with JSX</h1>
+      <h2>About</h2>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
+export default AboutPage
