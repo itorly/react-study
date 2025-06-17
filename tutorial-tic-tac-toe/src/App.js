7ccae029d6f8eb73dfe337469696f936096900f3
itorly <itorly@163.com>
2025-06-17 18:42:15 +0800

    call handleClick until the user clicks

--- tutorial-tic-tac-toe/src/App.js
+++ tutorial-tic-tac-toe/src/App.js
@@ -17,22 +17,28 @@ export default function Board() {
     setSquares(nextSquares);
   }
 
+  // When you were passing onSquareClick={handleClick}, you were passing the handleClick function down as a prop.
+  // When you were passing onSquareClick={handleClick(0)}, you were calling that function right away—notice the parentheses in handleClick(0)—and that’s why it runs too early.
+
+  // call handleClick until the user clicks:
+  // () => handleClick(0) is an arrow function, which is a shorter way to define functions. 
+  // When the square is clicked, the code after the => “arrow” will run, calling handleClick(0).
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
