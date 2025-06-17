147d6c86c1e7cc67eb151390ef3cf228e3167150
itorly <itorly@163.com>
2025-06-17 17:03:45 +0800

    Making an interactive component

--- tutorial-tic-tac-toe/src/App.js
+++ tutorial-tic-tac-toe/src/App.js
@@ -1,5 +1,16 @@
 function Square({ value=0 }) {
-  return <button className="square">{value}</button>;
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
