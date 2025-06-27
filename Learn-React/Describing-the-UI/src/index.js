6d47893515e3282da75f0ad7a1d07d48df87ec08
itorly <itorly@163.com>
2025-06-27 17:37:34 +0800

    3.Writing markup with JSX

--- Learn-React/Describing-the-UI/src/index.js
+++ Learn-React/Describing-the-UI/src/index.js
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import App_1_2 from "./App";
+import App_3 from "./App3";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <App_1_2 />
+    <App_3 />
   </StrictMode>
 );
