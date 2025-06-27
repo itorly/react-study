4a94c7ae5906aef32eb3e3d6197aa9fe1f168dee
itorly <itorly@163.com>
2025-06-27 18:13:32 +0800

    4.JavaScript in JSX with curly braces

--- Learn-React/Describing-the-UI/src/index.js
+++ Learn-React/Describing-the-UI/src/index.js
@@ -4,11 +4,13 @@ import "./styles.css";
 
 import App_1_2 from "./App";
 import App_3 from "./App3";
+import App_4 from "./App4";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App_1_2 />
     <App_3 />
+    <App_4 />
   </StrictMode>
 );
