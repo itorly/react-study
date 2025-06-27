0dd643b01af0a49a2779dcc65258c2fe621e5d17
itorly <itorly@163.com>
2025-06-27 23:07:17 +0800

    7.Rendering lists: app

---
+++ Learn-React/Describing-the-UI/src/App7_Rendering lists.js
@@ -0,0 +1,24 @@
+import { people } from './7.Rendering lists/data.js';
+import { getImageUrl } from './7.Rendering lists/utils.js';
+
+export default function List() {
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <ul>{listItems}</ul>
+    </article>
+  );
+}
--- Learn-React/Describing-the-UI/src/index.js
+++ Learn-React/Describing-the-UI/src/index.js
@@ -7,6 +7,7 @@ import App_3 from "./App3";
 import App_4 from "./App4";
 import App_5 from "./APP5_Passing props to a component"
 import App_6 from "./App6_Conditional rendering"
+import App_7 from "./App7_Rendering lists"
 
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -16,5 +17,6 @@ root.render(
     <App_4 />
     <App_5 />
     <App_6 />
+    <App_7 />
   </StrictMode>
 );
