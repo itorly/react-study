41cf627d4229a99c39fb5218a077c537b455f652
itorly <itorly@163.com>
2025-06-29 02:30:51 +0800

    using Image.js

--- Learn-React/Adding-Interactivity/src/index.js
+++ Learn-React/Adding-Interactivity/src/index.js
@@ -4,11 +4,13 @@ import "./styles.css";
 
 import App1 from "./App1_Responding to events";
 import App2 from "./App2_State-a memory of component";
+import Image from './3.Render and Commit/Image.js';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App1 />
     <App2 />
+    <Image />
   </StrictMode>
 );
