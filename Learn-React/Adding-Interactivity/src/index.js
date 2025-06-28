c52ca04159cbee10b21a531d1807e480833e0afa
itorly <itorly@163.com>
2025-06-29 02:43:53 +0800

    rendering: calling the Gallery component

--- Learn-React/Adding-Interactivity/src/index.js
+++ Learn-React/Adding-Interactivity/src/index.js
@@ -5,6 +5,7 @@ import "./styles.css";
 import App1 from "./App1_Responding to events";
 import App2 from "./App2_State-a memory of component";
 import Image from './3.Render and Commit/Image.js';
+import Gallery from './3.Render and Commit/Gallery.js';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -12,5 +13,6 @@ root.render(
     <App1 />
     <App2 />
     <Image />
+    <Gallery />
   </StrictMode>
 );
