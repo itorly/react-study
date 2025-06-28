12aff7f9a3209b8c7dcb70b32088c9dbd82f8dc3
itorly <itorly@163.com>
2025-06-28 20:49:57 +0800

    2.State: a component’s memory

--- Learn-React/Adding-Interactivity/src/index.js
+++ Learn-React/Adding-Interactivity/src/index.js
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import App1 from "./App1_Responding to events";
+import App2 from "./App2_State-a memory of component";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App1 />
+    <App2 />
   </StrictMode>
 );
