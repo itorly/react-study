d53f7fc49fa0f70345863292f2f4a095f1986c98
itorly <itorly@163.com>
2025-06-29 14:05:16 +0800

    Note: 3. Render and Commit

--- Learn-React/Adding-Interactivity/src/index.js
+++ Learn-React/Adding-Interactivity/src/index.js
@@ -8,6 +8,20 @@ import Image from './3.Render and Commit/Image.js';
 import Gallery from './3.Render and Commit/Gallery.js';
 import Clock from './3.Render and Commit/step3-React commits changes to the DOM/App.js'
 
+/**
+ * 3. Render and Commit
+ * Step 1: Trigger a render
+ *  done by calling createRoot with the target DOM node, 
+ * and then calling its render method with your component:
+ * 
+ * Step 2: React renders your components
+ * “Rendering” is React calling your components.
+ * 
+ * Step 3: React commits changes to the DOM.
+ * For the initial render, React will use the appendChild() DOM API to put all the DOM nodes it has created on screen.
+ * For re-renders, React will apply the minimal necessary operations (calculated while rendering!) to make the DOM match the latest rendering output.
+ * 
+ */
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
