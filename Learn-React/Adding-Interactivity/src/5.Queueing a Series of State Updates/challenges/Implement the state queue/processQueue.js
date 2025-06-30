e84dbb3fb89501c975032efb7d38f30e3e5c3352
itorly <itorly@163.com>
2025-06-30 15:05:35 +0800

    add the official solution

--- Learn-React/Adding-Interactivity/src/5.Queueing a Series of State Updates/challenges/Implement the state queue/processQueue.js
+++ Learn-React/Adding-Interactivity/src/5.Queueing a Series of State Updates/challenges/Implement the state queue/processQueue.js
@@ -1,6 +1,7 @@
 export function getFinalState(baseState, queue) {
   let finalState = baseState;
 
+  // my solution
   for (let i = 0; i < queue.length; i++) {
     const element = queue[i];
     if (element == 'n => n+1') {
@@ -9,6 +10,17 @@ export function getFinalState(baseState, queue) {
         finalState = element;
     }
   }
+
+  // official solution
+//   for (let update of queue) {
+//     if (typeof update === 'function') {
+//       // Apply the updater function.
+//       finalState = update(finalState);
+//     } else {
+//       // Replace the next state.
+//       finalState = update;
+//     }
+//   }
 
   return finalState;
 }
