25e0764d327e883cb57c397abfaab794e7ec3094
itorly <itorly@163.com>
2025-07-01 18:33:48 +0800

    Sample errors: push() will mutate an array

---
+++ Learn-React/Adding-Interactivity/src/7.Updating Arrays in State/7.1.Updating arrays without mutation/7.1.1.Adding to an array/App_Push.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+let nextId = 0;
+
+export default function List() {
+  const [name, setName] = useState('');
+  const [artists, setArtists] = useState([]);
+  
+  return (
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <input
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <button onClick={() => {
+        // Sample errors: push() will mutate an array, which you don’t want:
+        artists.push({
+          id: nextId++,
+          name: name,
+        });
+      }}>Add</button>
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
