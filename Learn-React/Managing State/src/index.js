fa4d3aad2d3af4f6972ab1926e18eaa3789fdfcf
itorly <itorly@163.com>
2025-07-04 16:58:08 +0800

    Step 5: Connect the event handlers to set state

--- Learn-React/Managing State/src/index.js
+++ Learn-React/Managing State/src/index.js
@@ -5,6 +5,7 @@ import "./styles.css";
 import App1 from "./1.Reacting to input with state/Step 1 Identify your components different visual states/App1";
 import App2 from "./1.Reacting to input with state/Step 1 Identify your components different visual states/App2";
 import DisplayingManyVisualStatesAtOnce from "./1.Reacting to input with state/Step 2 Displaying many visual states at once/DisplayingManyVisualStatesAtOnce";
+import ConnectTheEventHandlersToSetState from "./1.Reacting to input with state/Step 5 Connect the event handlers to set state/ConnectTheEventHandlersToSetState";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -19,6 +20,8 @@ root.render(
     <h4>Deep Dive</h4>
     <h5>Displaying many visual states at once</h5>
     <DisplayingManyVisualStatesAtOnce />
-
+    <hr/>
+    <h4>Step 5: Connect the event handlers to set state</h4>
+    <ConnectTheEventHandlersToSetState />
   </StrictMode>
 );
