f57eef48794b6533cff8e6143450f234907bfe75
itorly <itorly@163.com>
2025-07-15 15:26:48 +0800

    2.5.2.Editable State Duplicated

---
+++ Learn-React/Managing State/src/2.Choosing the state structure/2.5.Avoid duplication in state/2.5.2.Editable State Duplicated/AppStateDuplicatedAndEditable.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+const initialItems = [
+  { title: 'pretzels', id: 0 },
+  { title: 'crispy seaweed', id: 1 },
+  { title: 'granola bar', id: 2 },
+];
+
+/**
+ * Notice how if you first click “Choose” on an item and then edit it, 
+ * the input updates but the label at the bottom does not reflect the edits. 
+ * This is because you have duplicated state, 
+ * and you forgot to update selectedItem.
+ *
+ * Although you could update selectedItem too, an easier fix is to remove duplication.
+ */
+export default function MenuTwo() {
+  const [items, setItems] = useState(initialItems);
+  const [selectedItem, setSelectedItem] = useState(
+    items[0]
+  );
+
+  function handleItemChange(id, e) {
+    setItems(items.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          title: e.target.value,
+        };
+      } else {
+        return item;
+      }
+    }));
+  }
+
+  return (
+    <>
+      <h2>What's your travel snack?</h2> 
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            <input
+              value={item.title}
+              onChange={e => {
+                handleItemChange(item.id, e)
+              }}
+            />
+            {' '}
+            <button onClick={() => {
+              setSelectedItem(item);
+            }}>Choose</button>
+          </li>
+        ))}
+      </ul>
+      <p>You picked {selectedItem.title}.</p>
+    </>
+  );
+}
