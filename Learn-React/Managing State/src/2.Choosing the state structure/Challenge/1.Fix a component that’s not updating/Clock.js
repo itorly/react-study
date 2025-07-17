f4765c78ae3856f5f7514c9fb68d96a58f54300f
itorly <itorly@163.com>
2025-07-17 23:51:23 +0800

    Challenge 1.Fix a component that’s not updating: Clock.js

---
+++ Learn-React/Managing State/src/2.Choosing the state structure/Challenge/1.Fix a component that’s not updating/Clock.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function Clock(props) {
+//   const [color, setColor] = useState(props.color);
+  const color = props.color;
+  return (
+    <h1 style={{ color: color }}>
+      {props.time}
+    </h1>
+  );
+}
