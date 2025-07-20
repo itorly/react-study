cc0ad5315e04e169a4261c557927dc4181042bfd
itorly <itorly@163.com>
2025-07-20 14:43:27 +0800

    4.1.State is tied to a position in the render tree

---
+++ Learn-React/Managing State/src/4.Preserving and Resetting State/4.1.State is tied to a position in the render tree/AppRenderTwoPositions.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './styles.css'
+
+export default function AppRenderTwoPositions() {
+  const counter = <Counter />;
+  return (
+    <div>
+      {counter}
+      {counter}
+    </div>
+  );
+}
+
+function Counter() {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = 'counter';
+  if (hover) {
+    className += ' hover';
+  }
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>{score}</h1>
+      <button onClick={() => setScore(score + 1)}>
+        Add one
+      </button>
+    </div>
+  );
+}
