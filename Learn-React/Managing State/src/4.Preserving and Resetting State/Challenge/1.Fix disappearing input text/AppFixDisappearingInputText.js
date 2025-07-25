b80f02a9516e5cfcc455d541cdaf949eb8350666
itorly <itorly@163.com>
2025-07-26 00:22:29 +0800

    my solution of Challenge 1.Fix disappearing input text

--- Learn-React/Managing State/src/4.Preserving and Resetting State/Challenge/1.Fix disappearing input text/AppFixDisappearingInputText.js
+++ Learn-React/Managing State/src/4.Preserving and Resetting State/Challenge/1.Fix disappearing input text/AppFixDisappearingInputText.js
@@ -2,23 +2,13 @@ import { useState } from 'react';
 
 export default function AppFixDisappearingInputText() {
   const [showHint, setShowHint] = useState(false);
-  if (showHint) {
-    return (
-      <div>
-        <p><i>Hint: Your favorite city?</i></p>
-        <Form />
-        <button onClick={() => {
-          setShowHint(false);
-        }}>Hide hint</button>
-      </div>
-    );
-  }
   return (
     <div>
+      {(showHint) ? <p><i>Hint: Your favorite city?</i></p> : null}
       <Form />
       <button onClick={() => {
-        setShowHint(true);
-      }}>Show hint</button>
+        setShowHint(!showHint);
+      }}>{showHint ? "Hide" : "Show"} hint</button>
     </div>
   );
 }
