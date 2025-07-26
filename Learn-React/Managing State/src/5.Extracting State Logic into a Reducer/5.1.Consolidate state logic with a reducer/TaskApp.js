236a837fdb20c4884afd2656f5fd729717d9da71
itorly <itorly@163.com>
2025-07-26 23:58:25 +0800

    Step 2: Write a reducer function

--- Learn-React/Managing State/src/5.Extracting State Logic into a Reducer/5.1.Consolidate state logic with a reducer/TaskApp.js
+++ Learn-React/Managing State/src/5.Extracting State Logic into a Reducer/5.1.Consolidate state logic with a reducer/TaskApp.js
@@ -44,6 +44,31 @@ export default function TaskApp() {
   );
 }
 
+function tasksReducer(tasks, action) {
+  if (action.type === 'added') {
+    return [
+      ...tasks,
+      {
+        id: action.id,
+        text: action.text,
+        done: false,
+      },
+    ];
+  } else if (action.type === 'changed') {
+    return tasks.map((t) => {
+      if (t.id === action.task.id) {
+        return action.task;
+      } else {
+        return t;
+      }
+    });
+  } else if (action.type === 'deleted') {
+    return tasks.filter((t) => t.id !== action.id);
+  } else {
+    throw Error('Unknown action: ' + action.type);
+  }
+}
+
 let nextId = 3;
 const initialTasks = [
   {id: 0, text: 'Visit Kafka Museum', done: true},
