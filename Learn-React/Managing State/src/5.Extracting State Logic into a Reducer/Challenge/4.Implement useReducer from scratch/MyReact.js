902b9bd4d26336128903fe76139105961088a134
itorly <itorly@163.com>
2025-07-29 15:35:11 +0800

    solution of Challenge 4

--- Learn-React/Managing State/src/5.Extracting State Logic into a Reducer/Challenge/4.Implement useReducer from scratch/MyReact.js
+++ Learn-React/Managing State/src/5.Extracting State Logic into a Reducer/Challenge/4.Implement useReducer from scratch/MyReact.js
@@ -4,6 +4,10 @@ export function useReducer(reducer, initialState) {
   const [state, setState] = useState(initialState);
 
   // ???
+  function dispatch(action) {
+    const nextState = reducer(state, action);
+    setState(nextState);
+  }
 
   return [state, dispatch];
 }
