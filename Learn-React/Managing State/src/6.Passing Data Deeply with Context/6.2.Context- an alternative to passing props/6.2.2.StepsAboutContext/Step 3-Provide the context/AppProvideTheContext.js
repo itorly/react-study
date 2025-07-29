61788acdd5657d1390488c9dddf1762bb47199d7
itorly <itorly@163.com>
2025-07-30 00:32:36 +0800

    Step 3-Provide the context: LevelContext.js, Heading.js, AppProvideTheContext.js

---
+++ Learn-React/Managing State/src/6.Passing Data Deeply with Context/6.2.Context- an alternative to passing props/6.2.2.StepsAboutContext/Step 3-Provide the context/AppProvideTheContext.js
@@ -0,0 +1,26 @@
+import Heading from './Heading.js';
+import Section from './Section.js';
+import '../styles.css';
+
+export default function Page() {
+  return (
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
