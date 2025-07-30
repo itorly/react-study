ae4990e906cd104962d41d4a410dbc1987c73ff4
itorly <itorly@163.com>
2025-07-30 22:25:21 +0800

    Modify the logic for setting the value of the className attribute of the section tag

--- Learn-React/Managing State/src/6.Passing Data Deeply with Context/6.4.Context passes through intermediate components/Section.js
+++ Learn-React/Managing State/src/6.Passing Data Deeply with Context/6.4.Context passes through intermediate components/Section.js
@@ -5,8 +5,8 @@ export default function Section({ children, isFancy }) {
   const level = useContext(LevelContext);
   return (
     <section className={
-      'section ' +
-      (isFancy ? 'fancy' : '')
+      // 'section ' +
+      (isFancy ? 'fancy' : 'section')
     }>
       <LevelContext value={level + 1}>
         {children}
