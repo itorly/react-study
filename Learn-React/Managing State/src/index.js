f6df1a86802e8c85a07ae055648a9441a2b40259
itorly <itorly@163.com>
2025-07-30 20:50:11 +0800

    reference AppContextPassesthroughIntermediateComponents.js

--- Learn-React/Managing State/src/index.js
+++ Learn-React/Managing State/src/index.js
@@ -13,6 +13,7 @@ import AppCreateTheContext from "./6.Passing Data Deeply with Context/6.2.Contex
 import AppUseTheContext from "./6.Passing Data Deeply with Context/6.2.Context- an alternative to passing props/6.2.2.StepsAboutContext/Step 2-Use the Context/AppUseTheContext";
 import AppProvideTheContext from "./6.Passing Data Deeply with Context/6.2.Context- an alternative to passing props/6.2.2.StepsAboutContext/Step 3-Provide the context/AppProvideTheContext";
 import AppThSameComponentUsingAndProvidingContext from "./6.Passing Data Deeply with Context/6.3.Using and providing context from the same component/AppThSameComponentUsingAndProvidingContext";
+import AppContextPassesthroughIntermediateComponents from "./6.Passing Data Deeply with Context/6.4.Context passes through intermediate components/AppContextPassesthroughIntermediateComponents";
 
 import App1 from "./1.Reacting to input with state/Step 1 Identify your components different visual states/App1";
 import App2 from "./1.Reacting to input with state/Step 1 Identify your components different visual states/App2";
@@ -130,6 +131,18 @@ root.render(
     <hr/>
     <h3>6.3.Using and providing context from the same component</h3>
     <AppThSameComponentUsingAndProvidingContext />
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <hr/>
+    <h3>6.4.Context passes through intermediate components</h3>
+    <AppContextPassesthroughIntermediateComponents />
     <br/>
     <br/>
     <br/>
