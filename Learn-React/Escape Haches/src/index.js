de7ae62b32d437456f96c550aba71378e84eeb03
itorly <itorly@163.com>
2025-08-11 23:59:19 +0800

    import and render AppAddARef

--- Learn-React/Escape Haches/src/index.js
+++ Learn-React/Escape Haches/src/index.js
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import AppAddARef from "./1.Referencing Values with Refs/1.1.Adding a ref to your component/AppAddARef";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <h1>1.1.Adding a ref to your component</h1>
+    <AppAddARef />
   </StrictMode>
 );
