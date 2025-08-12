19171ff37ccdc6fcd9af556b694e9299dd02d5df
itorly <itorly@163.com>
2025-08-12 23:50:40 +0800

    import and render AppStopWatch

--- Learn-React/Escape Haches/src/index.js
+++ Learn-React/Escape Haches/src/index.js
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import AppAddARef from "./1.Referencing Values with Refs/1.1.Adding a ref to your component/AppAddARef";
+import Stopwatch from "./1.Referencing Values with Refs/1.2.Example- building a stopwatch/AppStopWatch";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <h1>1.1.Adding a ref to your component</h1>
+    <h2>Referencing Values with Refs</h2>
+    <h3>1.1.Adding a ref to your component</h3>
     <AppAddARef />
+    <h3>1.2.Example- building a stopwatch</h3>
+    <Stopwatch />
   </StrictMode>
 );
