4956b9182f4225f0902b107ba6bb927ea4ed2f49
itorly <itorly@163.com>
2025-08-21 22:03:39 +0800

    import and render components in Challenge

--- Learn-React/Escape Haches/src/index.js
+++ Learn-React/Escape Haches/src/index.js
@@ -7,6 +7,10 @@ import AppStopWatch from "./1.Referencing Values with Refs/1.2.Example- building
 import AppStopWatchWithStop from "./1.Referencing Values with Refs/1.2.Example- building a stopwatch/AppStopWatchWithStop";
 import AppCounterWithState from "./1.Referencing Values with Refs/1.3.Differences between refs and state/AppCounterWithState";
 import AppCounterWithRef from "./1.Referencing Values with Refs/1.3.Differences between refs and state/AppCounterWithRef";
+import AppFixBrokenChatInput from "./1.Referencing Values with Refs/Challenge/1.Fix a broken chat input/AppFixBrokenChatInput";
+import AppFixComponentFailingToReRender from "./1.Referencing Values with Refs/Challenge/2.Fix a component failing to re-render/AppFixComponentFailingToReRender";
+import AppFixDebouncing from "./1.Referencing Values with Refs/Challenge/3.Fix debouncing/AppFixDebouncing";
+import AppReadTheLatestState from "./1.Referencing Values with Refs/Challenge/4.Read the latest state/AppReadTheLatestState";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -20,5 +24,14 @@ root.render(
     <h3>1.3.Differences between refs and state</h3>
     <AppCounterWithState />
     <AppCounterWithRef />
+    <h3>Challenge</h3>
+    <h4>1.Fix a broken chat input</h4>
+    <AppFixBrokenChatInput />
+    <h4>2.Fix a component failing to re-render</h4>
+    <AppFixComponentFailingToReRender />
+    <h4>3.Fix debouncing</h4>
+    <AppFixDebouncing />
+    <h4>4.Read the latest state</h4>
+    <AppReadTheLatestState />
   </StrictMode>
 );
