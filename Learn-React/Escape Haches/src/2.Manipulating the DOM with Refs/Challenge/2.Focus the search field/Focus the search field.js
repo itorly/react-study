940aff32a2b3976b7173af1cfb8a6b4a38f9795d
itorly <itorly@163.com>
2025-08-28 17:02:08 +0800

    my solution of Challenge 2

--- Learn-React/Escape Haches/src/2.Manipulating the DOM with Refs/Challenge/2.Focus the search field/Focus the search field.js
+++ Learn-React/Escape Haches/src/2.Manipulating the DOM with Refs/Challenge/2.Focus the search field/Focus the search field.js
@@ -1,10 +1,20 @@
+import { useRef } from "react";
+
 export default function Page() {
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button onClick={handleClick}>
+          Search
+        </button>
       </nav>
-      <input
+      <input ref={inputRef}
         placeholder="Looking for something?"
       />
     </>
