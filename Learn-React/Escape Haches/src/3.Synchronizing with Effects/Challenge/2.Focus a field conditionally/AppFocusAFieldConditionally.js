b09e0caa0a0f330d7f4fb4599ba3a8aec478ced0
itorly <itorly@163.com>
2025-09-30 20:45:14 +0800

    Challenge 2.Focus a field conditionally

--- Learn-React/Escape Haches/src/3.Synchronizing with Effects/Challenge/2.Focus a field conditionally/AppFocusAFieldConditionally.js
+++ Learn-React/Escape Haches/src/3.Synchronizing with Effects/Challenge/2.Focus a field conditionally/AppFocusAFieldConditionally.js
@@ -1,19 +1,38 @@
-import { useEffect, useRef } from 'react';
+import { useState } from 'react';
+import MyInput from './MyInput.js';
 
-export default function MyInput({ shouldFocus, value, onChange }) {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    if (shouldFocus) {
-      ref.current.focus();
-    }
-  }, [shouldFocus]);
-
+export default function Form() {
+  const [show, setShow] = useState(false);
+  const [firstName, setFirstName] = useState('Taylor');
+  const [lastName, setLastName] = useState('Swift');
+  const [upper, setUpper] = useState(false);
+  const name = firstName + ' ' + lastName;
   return (
-    <input
-      ref={ref}
-      value={value}
-      onChange={onChange}
-    />
+    <>
+      <button onClick={() => setShow(s => !s)}>{show ? 'Hide' : 'Show'} form</button>
+      <br />
+      <hr />
+      {show && (
+        <>
+          <label>
+            Enter your first name:
+            <MyInput
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+              shouldFocus={true}
+            />
+          </label>
+          <label>
+            Enter your last name:
+            <MyInput
+              value={lastName}
+              onChange={e => setLastName(e.target.value)}
+              shouldFocus={false}
+            />
+          </label>
+          <p>Hello, <b>{upper ? name.toUpperCase() : name}</b></p>
+        </>
+      )}
+    </>
   );
 }
