a0feedf3a7198c93b40063993145a360f067b3aa
itorly <itorly@163.com>
2025-09-30 20:43:51 +0800

    Challenge 1.Focus a field on mount

--- Learn-React/Escape Haches/src/3.Synchronizing with Effects/Challenge/1.Focus a field on mount/AppFocusAFieldOnMount.js
+++ Learn-React/Escape Haches/src/3.Synchronizing with Effects/Challenge/1.Focus a field on mount/AppFocusAFieldOnMount.js
@@ -1,17 +1,35 @@
-import { useEffect, useRef } from 'react';
+import { useState } from 'react';
+import MyInput from './MyInput.js';
 
-export default function MyInput({ value, onChange }) {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    ref.current.focus();
-  }, []);
-
+export default function Form() {
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState('Taylor');
+  const [upper, setUpper] = useState(false);
   return (
-    <input
-      ref={ref}
-      value={value}
-      onChange={onChange}
-    />
+    <>
+      <button onClick={() => setShow(s => !s)}>{show ? 'Hide' : 'Show'} form</button>
+      <br />
+      <hr />
+      {show && (
+        <>
+          <label>
+            Enter your name:
+            <MyInput
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={upper}
+              onChange={e => setUpper(e.target.checked)}
+            />
+            Make it uppercase
+          </label>
+          <p>Hello, <b>{upper ? name.toUpperCase() : name}</b></p>
+        </>
+      )}
+    </>
   );
 }
