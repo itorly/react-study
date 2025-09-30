8a6dc1bd5ea29136830488b7c05a25b76d52a2e6
itorly <itorly@163.com>
2025-09-30 21:19:52 +0800

    explanation of solution of Challenge 3

--- Learn-React/Escape Haches/src/3.Synchronizing with Effects/Challenge/3.Fix an interval that fires twice/Counter.js
+++ Learn-React/Escape Haches/src/3.Synchronizing with Effects/Challenge/3.Fix an interval that fires twice/Counter.js
@@ -7,7 +7,23 @@ export default function Counter() {
     function onTick() {
       setCount(c => c + 1);
     }
+    /**
+     * When Strict Mode is on (like in the sandboxes on this site), 
+     * React remounts each component once in development. 
+     * 
+     * This causes the interval to be set up twice, 
+     * and this is why each second the counter increments twice.
 
+     * However, React’s behavior is not the cause of the bug: 
+     * the bug already exists in the code. 
+     * 
+     * React’s behavior makes the bug more noticeable. 
+     * The real cause is that this Effect starts a process 
+     * but doesn’t provide a way to clean it up.
+     *
+     * To fix this code, save the interval ID returned by setInterval, 
+     * and implement a cleanup function with clearInterval
+     */
     const intervalId = setInterval(onTick, 1000);
     return () => clearInterval(intervalId);
   }, []); 
